8ae2b9363e0ab1e6182440cc66c0d418ea726c25
Nigel Sebastian <nigelsebastian21@gmail.com>
2021-09-24 15:55:39 +0800

    9_24_2021

--- .github/workflows/JavaProgramming/Problem_analysis/Package.java
+++ .github/workflows/JavaProgramming/Problem_analysis/Package.java
@@ -6,7 +6,8 @@ public class Package {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         Scanner t_s = new Scanner(System.in);
-
+        double gross = 0.0;
+        double net = 0.0;
         int A = 500;
 
         System.out.print("Weight in Kg: ");
@@ -18,8 +19,6 @@ public class Package {
         System.out.print("Distance in Km: ");
         int d = inp.nextInt();
 
-        double gross = 0.0;
-        double net = 0.0;
         if (d <= 500) {
             if (t == 'A' || t == 'a') {
                 if (w <= 5 && w > 0) {
@@ -54,15 +53,15 @@ public class Package {
         if (d > 500) {
             if (t == 'A' || t == 'a') {
                 if (w <= 5 && w > 0) {
-                    gross = w * (10.50 * (d / A));
+                    gross = w + (10.50 * (d / A));
                     net = gross + (gross * 0.10);
                 }
                 if (w > 5 && w < 10) {
-                    gross = w * (15.75 * (d / A));
+                    gross = w + (15.75 * (d / A));
                     net = gross + (gross * 0.10);
                 }
                 if (w > 10 && w < 20) {
-                    gross = w * (20.65 * (d / A));
+                    gross = w + (20.65 * (d / A));
                     net = gross + (gross * 0.10);
                 }
             }
