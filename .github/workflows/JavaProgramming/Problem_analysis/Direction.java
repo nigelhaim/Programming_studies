bc7921f289e0b787bc349d458ccc32dda31aaa77
nigelhaim <84355145+nigelhaim@users.noreply.github.com>
2021-09-26 17:08:33 +0800

    09_26_2021

--- .github/workflows/JavaProgramming/Problem_analysis/Direction.java
+++ .github/workflows/JavaProgramming/Problem_analysis/Direction.java
@@ -8,22 +8,23 @@ public class Direction {
 
         System.out.print("True bearing: ");
         int t_t = di.nextInt();
-        int brng = 180 - t_t;
+        int brng = 0;
 
         if (brng > 0 && brng < 360) {
             if (brng > 0 && brng < 90) {
+      
                 System.out.print("N " + brng + " E");
             }
 
             if (brng > 90 && brng < 180) {
-                System.out.print("S " + brng + " E");
+                System.out.print("S " + (180 - brng) + " E");
             }
 
             if (brng > 180 && brng < 270) {
-                System.out.print("S" + brng + "W");
+                System.out.print("S" + (brng - 180)+ "W");
             }
             if (brng > 270 && brng < 360) {
-                System.out.print("N" + brng + "E");
+                System.out.print("N" + (360 - brng) + "E");
             }
         }
         di.close();
