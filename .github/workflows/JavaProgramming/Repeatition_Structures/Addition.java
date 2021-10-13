264760ddae27316fa3b56f6a736d31c3d6ef91f4
Nigel Sebastian <nigelsebastian21@gmail.com>
2021-10-13 13:11:18 +0800

    10_13_2021

---
+++ .github/workflows/JavaProgramming/Repeatition_Structures/Addition.java
@@ -0,0 +1,26 @@
+package Repeatition_Structures;
+
+import java.util.Scanner;
+
+public class Addition 
+{
+    public static void main(String[] args) 
+    {
+        Scanner inte = new Scanner(System.in);
+        
+        System.out.print("Input Integer: ");
+        int num  = inte.nextInt();
+        int add = 0;
+        String num_s = String.valueOf(num);
+        for(int x = 0; num_s.length() > x; x++)
+        {
+            System.out.print(num_s.charAt(x));
+            String num_i = Character.toString(num_s.charAt(x));
+            add += (Integer.parseInt(num_i));
+        }
+        System.out.println();
+        System.out.print("The total of the integers is: "+ add);
+        inte.close();
+
+    }    
+}
---
+++ .github/workflows/JavaProgramming/Repeatition_Structures/Reversed.java
@@ -0,0 +1,28 @@
+package Repeatition_Structures;
+
+import java.util.Scanner;
+
+public class Reversed 
+{
+    public static void main(String[] args) 
+    {
+        Scanner rev = new Scanner(System.in);
+        
+        System.out.print("Input Integer: ");
+        int num = rev.nextInt();
+
+        int d = 0 ;
+        for (int x = 0; num > 0;  x++)
+        {
+            int reversed = num % 10;
+            d = d * 10 + reversed;
+
+            num /= 10;
+
+           
+        }
+        System.out.print("The reversed version is: " + d);
+        rev.close();
+
+    }   
+}
