1b0a8247746f6f6095b2d06f44f349ffaab8641e
Nigel Sebastian <nigelsebastian21@gmail.com>
2021-10-27 21:01:57 +0800

    10_21_2021

--- .github/workflows/JavaProgramming/Machine_prob2/Machine_problem2_sebastian.java
+++ .github/workflows/JavaProgramming/Machine_prob2/Machine_problem2_sebastian.java
@@ -26,6 +26,8 @@ public class Machine_problem2_sebastian
         System.out.print("Please input what to display: ");
         char i = nhs.nextLine().toUpperCase().charAt(0);
 
+        //Initialize variable for harmonic and geometric programs 
+        char var = 'a';
         //Program proper
         System.out.println();
         //If user input A or a
@@ -53,12 +55,15 @@ public class Machine_problem2_sebastian
         
 
         System.out.println();
+        int n = 1;
         //Prompts for 3rd value 
-        System.out.println("The number of terms must be greater than 0");
-        System.out.print("Input the number of terms: ");
-        int n = nhs.nextInt();
-        System.out.println();
-        
+        if (i != 'C')
+        {
+            System.out.println("The number of terms must be greater than 0");
+            System.out.print("Input the number of terms: ");
+            n = nhs.nextInt();
+            System.out.println();
+        }      
         //N must be greater than 0
         if (n > 0)
         {
@@ -94,7 +99,81 @@ public class Machine_problem2_sebastian
                     System.out.println();
                     System.out.print("The final total is: " + total);
                     break;
+
+                case 'C':
+                    System.out.println("Harmonic Mean");
+                    total = (1 / a);
+                    int x = 1;
+                    int count_count = 0;
+                    System.out.println("To show the result of the harmonic mean, please input 0.");
+                    do 
+                    {
+                        System.out.print("Input Additional integer " + var + ": ");
+                        x = nhs.nextInt();
+                        if (x != 0)
+                        {
+                            total += (1 / x);
+                            count_count++;
+                        }
+                        var++;
+
+                    }
+                    while(x != 0);
+
+                    double final_ans = count_count / total;
+                    System.out.print("The harmonic mean is: " + final_ans);
+                    break;
                 
+                case 'D':
+                    System.out.println("Geometric mean");
+                    System.out.println();
+                    int y = 1;
+                    total = a;
+                    int counter_count = 0 ;
+                    System.out.println();
+                    System.out.println();
+                    System.out.println("To show the result of the harmonic mean, please input 0.");
+                    do
+                    {
+                        System.out.print("Input additional integer " + var + ": ");
+                        y = nhs.nextInt();
+                        if (y != 0)
+                        {
+                            total *= y;
+                        }
+                        var++;
+                        counter_count++;
+                    }
+                    while(y != 0);   
+                    double final_total = Math.pow(total, (1.0 / counter_count));
+                    System.out.println("The geometric mean is: " + final_total); 
+                    break;   
+                    
+                case 'E':
+                    System.out.println("Largest Prime number");
+                    System.out.println("Input 0 to get the result.");
+                    System.out.println();
+                    int t = a;
+                    int q = a;
+                    for (int cn = 2; cn <= a / 2; cn++)
+                    {
+                        System.out.print("Input the next integer " + var + ": ");
+                        t = nhs.nextInt();
+                        if (t != 0)
+                        {
+                            if (a % cn == 0 && t > q)
+                            {
+                                q = t;
+                            }
+                            var++;
+                        }
+                        else
+                        {
+                            System.out.print("The largest prime number is: " + q);
+                            break;
+                        }
+                    }
+                    break;
                 case 'Q':
                     System.out.print("Goodbye!");
                     System.out.println();
