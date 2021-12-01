9209a639250876cc0b0ce3c1d287972faa00557b
nigelhaim <nigelsebastian21@gmail.com>
2021-12-01 18:02:10 +0800

    12_1_2021_2d arrays

---
+++ .github/workflows/JavaProgramming/two_d_arrays_for_each/vowels.java
@@ -0,0 +1,12 @@
+package two_d_arrays_for_each;
+
+public class vowels 
+{
+    public static void main(String[] args) 
+    {
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+        
+        for (char ch : vowels)
+            System.out.println(ch);
+    }    
+}
