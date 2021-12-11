eb8ba9d1fdef2e54cf84feeac70955f7955be36f
nigelhaim <nigelsebastian21@gmail.com>
2021-12-11 15:28:01 +0800

    Finalized

---
+++ .github/workflows/UST_Files_MP_for_Submission_Java/Selection_Structures/Commission.java
@@ -0,0 +1,74 @@
+package Selection_Structures;
+
+import java.util.Scanner;
+
+public class Commission 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("ABC Tading");
+        System.out.println();  
+        
+        Scanner x = new Scanner (System.in);
+        System.out.print("Input Employee Type (R - Regular | C - Contractual): ");
+        char t = x.nextLine().toUpperCase().charAt(0);
+
+        System.out.print("Input Sales per Month: P ");
+        double s = x.nextDouble();
+
+        System.out.print("Input cash advacne (Max of P10,000): ");
+        double ca = x.nextDouble();
+        double coms = 0.00;
+        double sc = 0.00;
+        double nc = 0.000;
+        if (ca <= 10000 && ca > 0)
+        {
+            switch (t)
+            {
+                case 'R':
+
+                if (s > 0 && s <= 200000)
+                    sc = s * 0.05;
+                else if (s < 500000)
+                    sc = s * 0.0625;
+                else
+                    sc = s * 0.082;
+                coms = sc - ca;      
+                break;
+
+                case 'C':
+
+                if (s > 0 && s <= 200000)
+                    sc = s * 0.045;
+                else if (s < 500000)
+                    sc = s * 0.0575;
+                else
+                    sc = s * 0.075;
+                coms = sc - ca;      
+                break;
+
+                default:
+                    System.out.println("Invalid Employee type!");
+                
+            }
+        }
+        else
+        {
+            System.out.print("Invalid cash advance!");
+        }
+
+        if (coms < ca)
+        {
+            nc = ca - coms;
+        }
+
+        else 
+        {
+            nc = sc - (sc * 0.12) - ca;
+        }
+        System.out.println();
+        System.out.println("Gross Commission: P " + sc);
+        System.out.println("Net Commission: P " + nc);
+        x.close();
+    }    
+}
---
+++ .github/workflows/UST_Files_MP_for_Submission_Java/Selection_Structures/Hotel.java
@@ -0,0 +1,82 @@
+package Selection_Structures;
+
+import java.util.Scanner;
+
+public class Hotel 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Welcome to the XYZ hotel!");
+
+        Scanner x = new Scanner (System.in);
+
+        System.out.print("Input room type ( G – garden view, P – pool view, L – lake view): ");
+        char rt = x.nextLine().toUpperCase().charAt(0);
+
+        System.out.print("Will the guest have extra bed (Y/N)? ");
+        char q = x.nextLine().toUpperCase().charAt(0);
+
+        int begin = 0;
+        switch(q)
+        {
+            case 'Y':
+                System.out.print("How Many Beds? ");
+                int b = x.nextInt();
+
+                if(rt == 'G')
+                    begin = b * 500;
+                else if(rt == 'P')
+                    begin = b * 400;
+                else if(rt == 'L')
+                    begin = b * 700; 
+                break; 
+            case 'N':
+                begin = 0;
+                break;
+
+            default:
+                System.out.print("Invalid input of beds!");
+                System.exit(0);
+
+        }  
+
+        System.out.print("Number of Days: ");
+        int d = x.nextInt();
+        
+        double gross = 0.00;
+        switch(rt)
+        {
+
+            case 'G':
+                gross = d * (2000 + begin);
+                break;
+                            
+            case 'P':
+                gross = d * (1800 + begin);
+                break;
+
+            case 'L':
+                gross = d * (1500 + begin);
+                break;
+            
+            default:
+                System.out.println();
+                System.out.print("Invalid Room Type!");
+                System.exit(0);
+        }
+
+        if(d > 5)
+        {
+            gross = gross - (gross * 0.08);
+        }
+        System.out.println();
+        System.out.println("Total Gross = " + gross);
+
+        double net = gross + (gross * 0.12);
+
+        System.out.print("Net Charge = " + net);
+        System.out.println();
+
+        x.close();
+    }    
+}
