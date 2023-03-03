a5cd77e7ae200803a03f53f9cd7d046b2155b9d3
nigelhaim <nigelsebastian21@gmail.com>
2023-03-03 13:42:29 +0800

    Working caesar cipher with loop

--- UST_2Y2S/CaesarCipher/CaesarCipher.java
+++ UST_2Y2S/CaesarCipher/CaesarCipher.java
@@ -5,7 +5,18 @@ public class CaesarCipher{
 	public static String encrypt(String message){
 		String mesg = "";
 		for(int i = 0; i < message.length(); i++){
-			mesg += (char)(message.charAt(i)+key);
+			char character= message.charAt(i);
+			if((character >= 'a') && ((character +key) >= 'z') && (character  != ' ')){
+				mesg += (char)((96 - (122 - character ))+key);//ASCII DECIMAL VALUE OF a
+			}
+			else if((character < 'a') && ((character +key) >= 'Z') && (character  != ' ')){
+			//ASCII DECIMAL VALUE OF A
+				mesg += (char)((64 - (90 - character ))+key);
+			}
+			else{
+				mesg += (char)(character+key);
+			}
+
 		}
 		return mesg;
 	}
@@ -13,7 +24,18 @@ public class CaesarCipher{
 	public static String decrypt(String message){
 		String mesg = "";
 		for(int i = 0; i < message.length(); i++){
-			mesg += (char)(message.charAt(i)-key);
+			//mesg += (char)(message.charAt(i)-key);
+			char character = message.charAt(i);
+			if((character >= 'Z') && ((character - key) <= 'a') && (character  != ' ')){
+				mesg += (char)((122 + (character-key)) - 96);//ASCII DECIMAL VALUE OF a
+			}
+			else if(((character - key) <= 'A') && (character >= 'Z') &&(character  != ' ')){
+			//ASCII DECIMAL VALUE OF A
+				mesg += (char)((90 + (character-key)) - 64);
+			}
+			else{
+				mesg += (char)(character-key);
+			}
 		}
 		return mesg;
 	}
